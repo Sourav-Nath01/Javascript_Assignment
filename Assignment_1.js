dca43468a857bc9f0c8d5065190e78c8151ca33a
Sourav Nath <105435517+Sourav-Nath01@users.noreply.github.com>
2022-08-20 00:30:47 +0530

    Add files via upload

---
+++ Assignment_1.js
@@ -0,0 +1,31 @@
+//Question-1
+/*
+function check_leap_year(year){
+    if((year%100==0 && year%400==0) || year%4==0){
+        console.log(`${year} is a leap year`);
+    }
+    else{
+        console.log(`${year} is not a leap year`);
+    }
+}
+
+let year=2017;
+check_leap_year(year);
+*/
+//Question-2
+/*
+let fahrenheit_to_celsius=(temp)=>{return ((temp-32)/9)*5;}
+let Celsius_to_fahrenheit=(temp)=>{return ((9*temp)/32)+32;}
+var temp;
+temp=45;
+console.log(Celsius_to_fahrenheit(temp));
+*/
+//Question-3
+/*
+let num=0;
+let res=1;
+for(let i=1;i<=num;i++){
+res*=i;
+}
+console.log(`factorial of ${num} is ${res}`);
+*/
